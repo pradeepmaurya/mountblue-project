5ad94b8122ef7a44965a4c610a2a1e9d9d45271f
pradeepmaurya <mouryapradeep00@gmail.com>
2023-03-07 13:07:14 +0530

    project2 is working

---
+++ project2.cjs
@@ -0,0 +1,9 @@
+let invent = require('./cars');
+let inventory = invent.inventory;
+findByLastId(inventory);
+function findByLastId(inventory) {
+
+    let obj = inventory[inventory.length-1];
+    console.log('Lst car is a', );
+    // console.log('Last car is a', obj.car make goes here* *car model goes here*)
+}
